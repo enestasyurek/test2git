4c49b1245cc1d5f0f60e662afd0249e2ac262b93
Enes Taşyürek <tasyurekenes22@gmail.com>
2022-10-19 00:12:23 +0300

    sefa commit

---
+++ src/test/class1.java
@@ -0,0 +1,14 @@
+package test;
+
+public class class1 {
+
+    public static void print() {
+        System.out.println("Void type");
+
+    }
+
+    public int print2() {
+        return 1123;
+    }
+
+}
---
+++ src/test/ss.java
@@ -0,0 +1,17 @@
+package test;
+
+import test.class1.*;
+
+import static test.class1.*;
+
+public class ss {
+
+    public static void main(String[] args) {
+        print();
+        class1 c = new class1();
+        System.out.println(c.print2() + 1);
+    }
+
+}
+
+
