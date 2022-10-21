aa261b1535ff8a3d5a2fc6631958fce5a94dc628
Enes Taşyürek <tasyurekenes22@gmail.com>
2022-10-21 12:07:31 +0300

    sefa commit

---
+++ src/meeting/tetsclass.java
@@ -0,0 +1,8 @@
+package meeting;
+
+public class tetsclass {
+    public static void main(String[] args) {
+       student veli = new student("Veli", 18);
+       System.out.println(veli.name + " " + veli.age);
+    }
+}
